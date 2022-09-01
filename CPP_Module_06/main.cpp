117806e46b42733aa3dc4cb191b32b8f97972801
mclam <mclam@student.42wolfsburg.de>
2022-09-01 10:23:13 +0200

    Initial Commit

---
+++ CPP_Module_06/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPP_Module_06
+//
+//  Created by Maud Clam on 9/1/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
