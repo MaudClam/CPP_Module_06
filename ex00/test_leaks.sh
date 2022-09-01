6ec7f5db81d3337dc555c0781d76e65a0968841c
mclam <mclam@student.42wolfsburg.de>
2022-09-01 16:10:35 +0200

    Some modifications test-scripts

--- ex00/test_leaks.sh
+++ ex00/test_leaks.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+BOLD='\033[1m'
+NORMAL='\033[0m'
+
+printf $BOLD"./test.sh \"leaks --atExit -- \" > test_leaks.log\n"$NORMAL
 ./test.sh "leaks --atExit -- " > test_leaks.log
+
+printf $BOLD"cat test_leaks.log | grep \"leaks for\"\n"$NORMAL
 cat test_leaks.log | grep "leaks for"
+
+printf $BOLD"rm test_leaks.log\n"$NORMAL
 rm test_leaks.log
--- ex00/test_valgrind.sh
+++ ex00/test_valgrind.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-./test.sh "valgrind " > test_valgrind.log
+BOLD='\033[1m'
+NORMAL='\033[0m'
+
+printf $BOLD"./test.sh \"valgrind \" 2> test_valgrind.log\n"$NORMAL
+./test.sh "valgrind " 2> test_valgrind.log
+
+printf $BOLD"cat test_valgrind.log | grep \"in use at exit:\"\n"$NORMAL
 cat test_valgrind.log | grep "in use at exit:"
+
+printf $BOLD"rm test_valgrind.log"\n"$NORMAL
 rm test_valgrind.log
