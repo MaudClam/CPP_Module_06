be0f63b08160884b1fd093013239a2fc2353227d
mclam <mclam@student.42wolfsburg.de>
2022-09-01 16:24:04 +0200

    Some modifications test-scripts

--- ex00/test_valgrind.sh
+++ ex00/test_valgrind.sh
@@ -3,11 +3,11 @@
 BOLD='\033[1m'
 NORMAL='\033[0m'
 
-printf $BOLD"./test.sh \"valgrind \" 2> test_valgrind.log\n"$NORMAL
+printf $BOLD"\n./test.sh \"valgrind \" 2> test_valgrind.log\n"$NORMAL
 ./test.sh "valgrind " 2> test_valgrind.log
 
-printf $BOLD"cat test_valgrind.log | grep \"in use at exit:\"\n"$NORMAL
+printf $BOLD"\ncat test_valgrind.log | grep \"in use at exit:\"\n\n"$NORMAL
 cat test_valgrind.log | grep "in use at exit:"
 
-printf $BOLD"rm test_valgrind.log"\n"$NORMAL
+printf $BOLD"\nrm test_valgrind.log\n\n"$NORMAL
 rm test_valgrind.log
